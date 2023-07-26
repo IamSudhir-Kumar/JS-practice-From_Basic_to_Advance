e794249f9e95c0f7711fdaec2adadad01fc114db
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-07-26 17:32:17 +0530

    Update Day-2.js
    second problem on switch - case method

--- Day-2.js
+++ Day-2.js
@@ -24,3 +24,32 @@ switch (new Date().getDay()){
     day = "Saturday";
 }
 console.log(day);
+
+//if the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. If you have multiple inputs with the same output, you can represent them in a switch statement like this:
+/*---Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High*/
+function sequentialSizes(val) {
+  let answer = "";
+switch(val){
+  case 1:
+  case 2:
+  case 3:
+  return "Low";
+  break;
+  case 4:
+  case 5:
+  case 6:
+  return "Mid";
+  break;
+  case 7:
+  case 8:
+  case 9:
+  return "High";
+  break;
+}
+  return answer;
+}
+
+sequentialSizes(1);
