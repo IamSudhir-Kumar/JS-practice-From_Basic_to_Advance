269c7fd5d4010ff68e4c722e1a66be5b80bfb35d
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-07-28 09:08:28 +0530

    Update Day-3.js

--- Day-3.js
+++ Day-3.js
@@ -1,1 +1,11 @@
+//Functions (basic functions with parameters):
+//Write a function that takes two numbers as parameters and returns their sum.
 
+//ParseInt is used to convert string into number (Parses a string and returns an integer)
+const a = parseInt(prompt("write the first number:"));
+const  b = parseInt(prompt("write the secomd number"));
+ function sum(a , b){
+   return a + b;
+ }
+const add = sum(a,b);
+document.getElementById("new").innerHTML = add;
