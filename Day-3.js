65d32a280101489ea0ce185d2121c3a1c24b23f7
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-07-28 10:52:59 +0530

    Update Day-3.js

--- Day-3.js
+++ Day-3.js
@@ -9,3 +9,10 @@ const  b = parseInt(prompt("write the secomd number"));
  }
 const add = sum(a,b);
 document.getElementById("new").innerHTML = add;
+
+//Implement a function to check if a given number is even or odd and return the result by using input
+function checkOddEven(){
+  const num = parseInt(document.getElementById("input").value);
+  const result = num % 2 === 0 ? "even" : "odd";
+  document.getElementById("new").innerHTML = result;
+}
