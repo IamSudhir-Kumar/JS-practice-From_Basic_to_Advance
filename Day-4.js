10fd193a29dd3c3c2e59eb83abb1b0d87cd3124b
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-02 21:56:49 +0530

    Update Day-4.js

--- Day-4.js
+++ Day-4.js
@@ -1,1 +1,23 @@
+/**Arrays (creating, accessing elements, array methods):**
 
+1. Create an array of your favorite fruits and display the third element in the console.
+2. Write a program to add an element to the end of an existing array using the push() method.
+3. Implement a program that uses the pop() method to remove the last element from an array.
+
+**Array Iteration (forEach, map, filter, reduce):**
+
+1. Use the forEach() method to loop through an array and display each element in the console.
+2. Create a program that uses the map() method to double each number in an array.
+3. Implement a program that uses the filter() method to find even numbers in an array.
+
+**Objects (creating, accessing properties, object methods):**
+
+1. Create an object representing a person with properties like name, age, and gender.
+2. Write a program that accesses and displays the value of a property from the created object.
+3. Implement a program that defines an object method to display the person's information.
+
+**Object Constructors and Prototypes:**
+
+1. Define a constructor function for creating a Car object with properties like make, model, and year.
+2. Create instances of Car using the constructor function and display their details.
+3. Implement a program that adds a method to the Car prototype to calculate the age of the car.*/
