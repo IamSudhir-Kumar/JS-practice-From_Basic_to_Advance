16a9edae1d317a4e0e1b0179ea733bd777b8222c
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-03 21:46:42 +0530

    Update Day-5.js

--- Day-5.js
+++ Day-5.js
@@ -1,1 +1,6 @@
+// Day-5
+// DOM Manipulation (selecting elements, modifying content)
+// Event Handling (click, submit, keypress, etc.)
+// Form Validation (basic form validation using JavaScript)
+
 
