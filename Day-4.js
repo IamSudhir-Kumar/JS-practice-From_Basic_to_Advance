517346e5ce59a59c441f186cfcf16e8326fd97e3
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-13 11:17:52 +0530

    Arrays (creating, accessing elements, array methods)

--- Day-4.js
+++ Day-4.js
@@ -1,23 +1,33 @@
-/**Arrays (creating, accessing elements, array methods):**
+// Arrays (creating, accessing elements, array methods):
 
-1. Create an array of your favorite fruits and display the third element in the console.
-2. Write a program to add an element to the end of an existing array using the push() method.
-3. Implement a program that uses the pop() method to remove the last element from an array.
+// 1. Create an array of your favorite fruits and display the third element in the console.
+const fruits = ["mangoe" , "apple", "orange", "kiwi"]
+console.log(fruits[2]);
 
-**Array Iteration (forEach, map, filter, reduce):**
+// 2. Write a program to add an element to the end of an existing array using the push() method.
+const oNe = fruits.push("Banana"); //using Push for adding element in last in an array
+const tWo = fruits.unshift("Papaya"); // using unshift for adding element in first in an array
 
-1. Use the forEach() method to loop through an array and display each element in the console.
-2. Create a program that uses the map() method to double each number in an array.
-3. Implement a program that uses the filter() method to find even numbers in an array.
+// 3. Implement a program that uses the pop() method to remove the last element from an array.
+const thRee = fruits.pop();//using Pop for removing element in last in an array
+const fOur = fruits.shift();//using Shift for removing element in first in an array
+console.log(fruits);
 
-**Objects (creating, accessing properties, object methods):**
 
-1. Create an object representing a person with properties like name, age, and gender.
-2. Write a program that accesses and displays the value of a property from the created object.
-3. Implement a program that defines an object method to display the person's information.
+// **Array Iteration (forEach, map, filter, reduce):**
 
-**Object Constructors and Prototypes:**
+// 1. Use the forEach() method to loop through an array and display each element in the console.
+// 2. Create a program that uses the map() method to double each number in an array.
+// 3. Implement a program that uses the filter() method to find even numbers in an array.
 
-1. Define a constructor function for creating a Car object with properties like make, model, and year.
-2. Create instances of Car using the constructor function and display their details.
-3. Implement a program that adds a method to the Car prototype to calculate the age of the car.*/
+// **Objects (creating, accessing properties, object methods):**
+
+// 1. Create an object representing a person with properties like name, age, and gender.
+// 2. Write a program that accesses and displays the value of a property from the created object.
+// 3. Implement a program that defines an object method to display the person's information.
+
+// **Object Constructors and Prototypes:**
+
+// 1. Define a constructor function for creating a Car object with properties like make, model, and year.
+// 2. Create instances of Car using the constructor function and display their details.
+// 3. Implement a program that adds a method to the Car prototype to calculate the age of the car.*/
