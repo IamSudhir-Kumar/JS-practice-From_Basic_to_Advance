31573fd949e432d50fda3642d359673003e203f8
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-13 12:12:55 +0530

    Objects (creating, accessing properties, object methods):

--- Day-4.js
+++ Day-4.js
@@ -13,18 +13,46 @@ const thRee = fruits.pop();//using Pop for removing element in last in an array
 const fOur = fruits.shift();//using Shift for removing element in first in an array
 console.log(fruits);
 
-
 // **Array Iteration (forEach, map, filter, reduce):**
 
 // 1. Use the forEach() method to loop through an array and display each element in the console.
-// 2. Create a program that uses the map() method to double each number in an array.
-// 3. Implement a program that uses the filter() method to find even numbers in an array.
+const arr = [77, 78, 59];
+arr.forEach(function(element){
+  console.log(element);
+})
 
-// **Objects (creating, accessing properties, object methods):**
+// 2. Create a program that uses the map() method to double each number in an array.
+function double(num){
+  return num*2;
+}
+console.log(arr.map(double));
+
+// 3. Implement a program that uses the filter() method to find even numbers in an array.
+function even(num){
+  return num%2 === 0;
+}
+console.log(arr.filter(even));
+
+// Objects (creating, accessing properties, object methods):
 
 // 1. Create an object representing a person with properties like name, age, and gender.
-// 2. Write a program that accesses and displays the value of a property from the created object.
-// 3. Implement a program that defines an object method to display the person's information.
+const info = {
+    name : "Jhon Doe",
+    age : 24,
+    gender : "male" ,
+  }
+  
+  // 2. Write a program that accesses and displays the value of a property from the created object.
+  // 3. Implement a program that defines an object method to display the person's information.
+  function print1(property){
+    console.log(info[property]);
+  };
+  function print2 (){
+      print1("name");
+      print1("age");
+      print1("gender");
+  }
+  print2();
 
 // **Object Constructors and Prototypes:**
 
