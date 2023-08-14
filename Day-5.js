715162435d8ecef7d1d816c8fb60b4e062d1883e
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-14 09:40:15 +0530

    topics assign

--- Day-5.js
+++ Day-5.js
@@ -2,3 +2,51 @@
 // String Manipulation (reversing, splitting, substrings)
 // JSON Manipulation (parse, stringify)
 // Math Functions (Math.random, Math.floor, etc.)
+// Certainly! Here are some basic programs to help you master the topics of String Manipulation, JSON Manipulation, and Math Functions:
+
+// **String Manipulation:**
+
+// 1. **Reverse a String:**
+//    Write a program that takes a string as input and outputs the reverse of that string.
+
+// 2. **Count Vowels and Consonants:**
+//    Create a program that takes a string as input and counts the number of vowels and consonants.
+
+// 3. **Check Palindrome:**
+//    Implement a program that checks if a given string is a palindrome (reads the same forwards and backwards).
+
+// 4. **Capitalize Words:**
+//    Write a program that takes a sentence as input and capitalizes the first letter of each word.
+
+// 5. **String Encryption:**
+//    Create a program that encrypts a string by shifting the characters by a certain number of positions in the alphabet.
+
+// **JSON Manipulation:**
+
+// 1. **Parse JSON:**
+//    Write a program that takes a JSON string as input and parses it into a JavaScript object. Then, access and display specific properties from the object.
+
+// 2. **Stringify JSON:**
+//    Create an object with various properties and values. Convert the object into a JSON string using the `JSON.stringify()` function.
+
+// 3. **Manipulate JSON Data:**
+//    Use parsed JSON data to perform calculations or display specific information. For instance, create a program that calculates the total value of items in a shopping cart.
+
+// **Math Functions:**
+
+// 1. **Generate Random Numbers:**
+//    Write a program that generates a random number between a specified range using `Math.random()`.
+
+// 2. **Round Numbers:**
+//    Create a program that takes a floating-point number as input and rounds it to the nearest integer using `Math.round()`.
+
+// 3. **Calculate Hypotenuse:**
+//    Implement a program that calculates the hypotenuse of a right triangle given the lengths of the two other sides using the Pythagorean theorem and `Math.sqrt()`.
+
+// 4. **Calculate Area and Circumference:**
+//    Write a program that calculates the area and circumference of a circle given its radius using `Math.PI` and relevant formulas.
+
+// 5. **Generate Random Password:**
+//    Create a program that generates a random password consisting of letters, numbers, and special characters.
+
+// Remember, the key to mastering these topics is practice. Experiment with different scenarios, modify the programs, and create your own variations. As you work on more complex problems, you'll gain a deeper understanding of how to manipulate strings, work with JSON data, and leverage mathematical functions effectively in your JavaScript code. Happy coding!
