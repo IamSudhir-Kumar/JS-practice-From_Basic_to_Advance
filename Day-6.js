83eecdcfdca0d6ec44efcb631890cab4c4a6cc55
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-15 21:51:09 +0530

    tasks for Nested loops

--- Day-6.js
+++ Day-6.js
@@ -31,3 +31,50 @@
 //     Given multiple arrays, combine them all into a single array using the spread operator.
 
 // These problems cover various scenarios where the spread operator can be used effectively. Try solving these problems, and it will give you practical experience with the spread operator and its applications in different contexts.
+// Sure, here are some nested loop problems that range from basic to more advanced. Working on these problems will help you master the concept of nested loops:
+
+// ### Basic Level:
+
+// 1. **Print Patterns:**
+//    Write a program to print various patterns using nested loops, such as squares, triangles, and diamond shapes.
+
+// 2. **Multiplication Table:**
+//    Print the multiplication table for numbers from 1 to 10 using nested loops.
+
+// 3. **Print Pascal's Triangle:**
+//    Write a program to print the first `n` rows of Pascal's Triangle using nested loops.
+
+// ### Intermediate Level:
+
+// 4. **Print Prime Numbers:**
+//    Write a program to print all prime numbers between 1 and 100 using nested loops.
+
+// 5. **Sum of Matrix Elements:**
+//    Given a matrix (2D array), find and print the sum of all elements using nested loops.
+
+// 6. **Find Duplicate Elements:**
+//    Given an array, find and print any duplicate elements using nested loops.
+
+// ### Advanced Level:
+
+// 7. **Matrix Multiplication:**
+//    Implement matrix multiplication for two matrices using nested loops.
+
+// 8. **Sudoku Solver:**
+//    Write a program to solve a Sudoku puzzle using nested loops and backtracking.
+
+// 9. **Prime Factorization:**
+//    Given a number, find and print its prime factorization using nested loops.
+
+// ### Expert Level:
+
+// 10. **N-Queens Problem:**
+//     Implement a solution to the N-Queens problem using nested loops and backtracking.
+
+// 11. **Generate Permutations:**
+//     Generate all permutations of a given set of elements using nested loops.
+
+// 12. **Image Convolution:**
+//     Implement a basic image convolution operation using nested loops for image processing.
+
+// // These problems cover a range of difficulty levels and applications for nested loops. As you work on these problems, you'll gain a deeper understanding of how to structure and use nested loops effectively in various scenarios.
