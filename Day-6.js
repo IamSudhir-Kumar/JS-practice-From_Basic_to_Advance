5afd265a42f8ca87f24fec9ba3e89d9c2abf1992
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-15 20:53:04 +0530

    spread Operator

--- Day-6.js
+++ Day-6.js
@@ -1,1 +1,33 @@
+// Certainly! Here are some problems involving the spread operator that can help you understand it better:
 
+// 1. **Merge Arrays:**
+//    Given two arrays, merge them using the spread operator and create a new array containing all elements.
+
+// 2. **Copy and Modify Object:**
+//    Given an object with properties, create a shallow copy of the object using the spread operator. Then, modify a property in the copied object.
+
+// 3. **Pass Arguments:**
+//    Create a function that takes three arguments and returns the sum of those numbers. Use the spread operator to pass an array of numbers to the function.
+
+// 4. **Clone Array and Append Element:**
+//    Given an array, create a clone of the array and then append an element to the clone using the spread operator.
+
+// 5. **Combine Object Properties:**
+//    Given two objects with properties, combine them into a single object using the spread operator.
+
+// 6. **Remove Element from Array:**
+//    Given an array and an element, create a new array that excludes the specified element using the spread operator.
+
+// 7. **Insert Element in the Middle of Array:**
+//    Given an array and an element, insert the element in the middle of the array using the spread operator.
+
+// 8. **Replace Element in Array:**
+//    Given an array and an element to replace, create a new array where the specified element is replaced by a new element using the spread operator.
+
+// 9. **Clone Array of Objects:**
+//    Given an array of objects, create a clone of the array and then modify a property in one of the objects using the spread operator.
+
+// 10. **Combine Multiple Arrays:**
+//     Given multiple arrays, combine them all into a single array using the spread operator.
+
+// These problems cover various scenarios where the spread operator can be used effectively. Try solving these problems, and it will give you practical experience with the spread operator and its applications in different contexts.
