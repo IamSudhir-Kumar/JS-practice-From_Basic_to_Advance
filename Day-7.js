7669b5d96f58d2a90d1abf5ed5a22884413a586d
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-28 14:04:48 +0530

    Error Handling (try-catch blocks)

--- Day-7.js
+++ Day-7.js
@@ -19,5 +19,7 @@ catch (error) {
 // Problem 2: Invalid Input
 // Create a function that prompts the user to enter their age. Use a try and catch block to handle scenarios where the user enters a non-numeric value. Display an error message in the catch block.
 
+// Problem 3: File Reading
+// Imagine you're reading data from a file using JavaScript (this is typically done on the server-side, but for the sake of practice, assume it's in a browser environment). Write a function that simulates reading a file and throws an error if the file is not found. Use a try and catch block to catch and display the error message.
 // Timing Events (setTimeout, setInterval)
 // Date and Time Manipulation
