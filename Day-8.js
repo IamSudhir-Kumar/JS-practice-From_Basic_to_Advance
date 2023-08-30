09449915421cf08940e22730f6a87fb933a0d774
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-08-30 20:52:47 +0530

    Update Day-8.js

--- Day-8.js
+++ Day-8.js
@@ -1,1 +1,11 @@
+// Problem 1: Counting Letters
 
+// Write a program that takes a string as input and counts the occurrences of each letter (case-insensitive). Use a Map to store the letter as the key and the count as the value. Display the letter count for each letter.
+
+// Problem 2: Removing Duplicate Numbers
+
+// Create a function that takes an array of numbers and returns a new array with duplicate numbers removed. Use a Set to efficiently track unique values.
+
+// Problem 3: User List with Set
+
+// Design a user registration system where users enter their names. Implement a system that ensures only unique names are stored. Use a Set to manage the list of unique names.
