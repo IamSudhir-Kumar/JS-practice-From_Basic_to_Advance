c77adee2f8be075a0cd9173b7dcc3f195f29de5a
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-09 13:25:08 +0530

    add a new project

---
+++ DSA with JS/easy/PalindromeCheck.js
@@ -0,0 +1,4 @@
+/* Create a function that checks if a given string is
+ the same when read forwards and backward (e.g., "racecar" or "level").
+  You can remove spaces and convert characters to lowercase for a simple 
+  solution.*/
