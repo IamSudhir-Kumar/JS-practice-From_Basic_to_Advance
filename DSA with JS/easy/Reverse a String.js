db33e693722b9c8c53d859a7e9a76664c3ef92a5
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-09 13:23:53 +0530

    Add function to reverse a string

--- DSA with JS/easy/Reverse a String.js
+++ DSA with JS/easy/Reverse a String.js
@@ -1,1 +1,9 @@
-// Reverse a String: Write a function that takes a string as input and returns the string reversed. You can iterate through the string in reverse order and build the reversed string character by character.
+/* Reverse a String: Write a function that takes a string as 
+input and returns the string reversed. You can iterate through
+the string in reverse order and build the reversed string character
+by character.*/
+function rev(inputStirng){
+    return inputStirng.split("").reverse().join("");
+}
+console.log(rev("Hello World!"));
+
