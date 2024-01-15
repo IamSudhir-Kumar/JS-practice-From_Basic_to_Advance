730eed633abd76c98090397a52735745578f6145
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-01-15 20:56:27 +0530

    Update index.js

--- OOP-with-JS/Task-7(inheritance)/index.js
+++ OOP-with-JS/Task-7(inheritance)/index.js
@@ -1,1 +1,6 @@
+// Create a class "PARENT" with a method print that prints "this is a parent class" and its Child class "CHILD" with a method print that prints "this is a child class". Now, create an object for each of the class and call
+// 1 - method of parent class by object of parent class
+// 2 - method of child class by object of child class
+// 3 - method of parent class by object of child class
 
+//solution///
