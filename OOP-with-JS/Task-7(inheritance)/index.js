6be39bee20fd2c7c3e6a6b4385ebda3a4b54e67f
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-01-15 21:05:57 +0530

    Update index.js

--- OOP-with-JS/Task-7(inheritance)/index.js
+++ OOP-with-JS/Task-7(inheritance)/index.js
@@ -4,3 +4,25 @@
 // 3 - method of parent class by object of child class
 
 //solution///
+//Write the code Below
+class PARENT {
+    print(){
+        console.log("this is a parent class");
+    }
+}
+class CHILD extends PARENT {
+    print(){
+        console.log("this is a child class");
+      super.print()
+    }
+}  
+
+//Do Not change the Below  Code
+
+
+
+var obj_parent = new PARENT();
+obj_parent.print();
+
+var obj_child = new CHILD();
+obj_child.print();
