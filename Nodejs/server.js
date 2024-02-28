9ca2ade28befc784dd26957ba0aa0cb179643f35
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-02-28 15:48:49 +0530

    node js

---
+++ Nodejs/server.js
@@ -0,0 +1,15 @@
+const os = require('os')
+const {add , subtract, divide, multiply} = require("./MAth")
+const path = require('path')
+console.log(os.type())
+console.log(os.version())
+
+console.log(add(9 , 5))
+console.log(subtract(7 , 8))
+console.log(divide(4 , 5))
+console.log(multiply(4, 7))
+
+console.log(__dirname)
+console.log(__filename)
+console.log(path.basename(__filename))
+console.log(path.parse(__filename))
